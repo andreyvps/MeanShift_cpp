d516a4a633230257999496b1cf771570d7b33dc5
Andrey Sklyar <andrey@victoryparadesoftware.com>
2016-07-29 16:22:16 -0700

    Find modes after finished shifting means.

--- MeanShift.cpp
+++ MeanShift.cpp
@@ -4,148 +4,123 @@
 
 using namespace std;
 
-double euclidean_distance(const vector<double> &point_a, const vector<double> &point_b){
-    double total = 0;
-    for(int i=0; i<point_a.size(); i++){
-        total += (point_a[i] - point_b[i]) * (point_a[i] - point_b[i]);
+namespace meanshift {
+    double euclidean_distance(const Point &point_a, const Point &point_b){
+        double total = 0;
+        for (int i = 0; i < point_a.size(); i++){
+            total += (point_a[i] - point_b[i]) * (point_a[i] - point_b[i]);
+        }
+        return sqrt(total);
     }
-    return sqrt(total);
-}
 
-double gaussian_kernel(double distance, double kernel_bandwidth){
-    double temp =  exp(-(distance*distance) / (kernel_bandwidth));
-    return temp;
-}
-
-void MeanShift::set_kernel( double (*_kernel_func)(double,double) ) {
-    if(!_kernel_func){
-        kernel_func = gaussian_kernel;
-    } else {
-        kernel_func = _kernel_func;    
+    double gaussian_kernel(double distance, double kernel_bandwidth){
+        double temp = exp(-(distance*distance) / (kernel_bandwidth));
+        return temp;
     }
-}
 
-vector<Cluster> MeanShift::cluster(
-    const vector<vector<double> > & points,
-    const double & kernel_bandwidth,
-    const vector<vector<double> > & probes)
-{
-    vector<vector<double>> mutable_probes = probes;
-    meanshift(points, kernel_bandwidth, mutable_probes);
-    vector<vector<double>> modes = find_modes(mutable_probes);
-    return cluster(points, modes);
-}
-
-void MeanShift::meanshift(
-    const std::vector<std::vector<double> > & points,
-    const double & kernel_bandwidth,
-    std::vector<std::vector<double> > & probes) 
-{
-    vector<bool> stop_moving(probes.size(), false);
-    double max_shift_distance;
-    do {
-        max_shift_distance = shift_probes(points, kernel_bandwidth, probes, stop_moving);
-        printf("max_shift_distance: %f\n", max_shift_distance);
-    } while (max_shift_distance > epsilon);
-}
-
-// Shift the probes to their local modes
-double MeanShift::shift_probes(
-    const std::vector<std::vector<double> > & points,
-    const double & kernel_bandwidth,
-    std::vector<std::vector<double> > & probes,
-    vector<bool> & stop_moving)
-{
-    double max_shift_distance = 0;
-    for (int i = 0; i<probes.size(); i++){
-        if (!stop_moving[i]) {
-            double shift_distance = shift_probe(points, kernel_bandwidth, probes[i]);
-            if (shift_distance > max_shift_distance){
-                max_shift_distance = shift_distance;
-            }
-            if (shift_distance <= epsilon) {
-                stop_moving[i] = true;
-            }
+    void MeanShift::set_kernel(double(*_kernel_func)(double, double)) {
+        if (!_kernel_func){
+            kernel_func = gaussian_kernel;
+        }
+        else {
+            kernel_func = _kernel_func;
         }
     }
-    return max_shift_distance;
-}
 
-double MeanShift::shift_probe(
-    const std::vector<std::vector<double> > & points,
-    const double & kernel_bandwidth,
-    std::vector<double> & probe)
-{
-    vector<double> shifted_point(probe.size(), 0);
-    double total_weight = 0;
-    for(int i=0; i<points.size(); i++){
-        vector<double> temp_point = points[i];
-        double distance = euclidean_distance(probe, temp_point);
-        double weight = kernel_func(distance, kernel_bandwidth);
-        for(int j=0; j<shifted_point.size(); j++){
-            shifted_point[j] += temp_point[j] * weight;
-        }
-        total_weight += weight;
+    vector<Point> MeanShift::find_modes(
+        const vector<Point> & points,
+        const double & kernel_bandwidth,
+        vector<Point> & probes)
+    {
+        meanshift(points, kernel_bandwidth, probes);
+        return find_modes(probes);
     }
 
-    for(int i=0; i<shifted_point.size(); i++){
-        shifted_point[i] /= total_weight;
+    void MeanShift::meanshift(
+        const vector<Point> & points,
+        const double & kernel_bandwidth,
+        vector<Point> & probes)
+    {
+        vector<bool> stop_moving(probes.size(), false);
+        double max_shift_distance;
+        do {
+            max_shift_distance = shift_probes(points, kernel_bandwidth, probes, stop_moving);
+            // printf("max_shift_distance: %f\n", max_shift_distance);
+        } while (max_shift_distance > epsilon);
     }
 
-    double distance = euclidean_distance(probe, shifted_point);
-    for (int i = 0; i < shifted_point.size(); i++) {
-        probe[i] = shifted_point[i];
-    }
-
-    return distance;
-}
-
-std::vector<vector<double> > MeanShift::find_modes(
-    const std::vector<std::vector<double> > & converged_probes)
-{
-    std::vector<vector<double> > modes;
-
-    for (int i = 0; i < converged_probes.size(); i++) {
-        vector<double> probe = converged_probes[i];
-
-        int m = 0;
-        for (; m < modes.size(); m++) {
-            if (euclidean_distance(modes[m], probe) < 2 * epsilon) {
-                break;
+    // Shift the probes to their local modes
+    double MeanShift::shift_probes(
+        const vector<Point> & points,
+        const double & kernel_bandwidth,
+        vector<Point> & probes,
+        vector<bool> & stop_moving)
+    {
+        double max_shift_distance = 0;
+        for (int i = 0; i < probes.size(); i++){
+            if (!stop_moving[i]) {
+                double shift_distance = shift_probe(points, kernel_bandwidth, probes[i]);
+                if (shift_distance > max_shift_distance){
+                    max_shift_distance = shift_distance;
+                }
+                if (shift_distance <= epsilon) {
+                    stop_moving[i] = true;
+                }
             }
         }
+        return max_shift_distance;
+    }
 
-        if (m == modes.size()) {
-            modes.push_back(probe);
+    double MeanShift::shift_probe(
+        const vector<Point> & points,
+        const double & kernel_bandwidth,
+        Point& probe)
+    {
+        Point shifted_point(probe.size(), 0);
+        double total_weight = 0;
+        for (int i = 0; i < points.size(); i++){
+            Point temp_point = points[i];
+            double distance = euclidean_distance(probe, temp_point);
+            double weight = kernel_func(distance, kernel_bandwidth);
+            for (int j = 0; j < shifted_point.size(); j++){
+                shifted_point[j] += temp_point[j] * weight;
+            }
+            total_weight += weight;
         }
+
+        for (int i = 0; i < shifted_point.size(); i++){
+            shifted_point[i] /= total_weight;
+        }
+
+        double distance = euclidean_distance(probe, shifted_point);
+        for (int i = 0; i < shifted_point.size(); i++) {
+            probe[i] = shifted_point[i];
+        }
+
+        return distance;
     }
 
-    return modes;
-}
+    vector<Point> MeanShift::find_modes(
+        const vector<Point> & converged_probes)
+    {
+        vector<Point> modes;
 
-vector<Cluster> MeanShift::cluster(
-    const vector<vector<double> > & points,
-    const vector<vector<double> > & modes)
-{
-    vector<Cluster> clusters(modes.size());
-    for (int m = 0; m < modes.size(); m++) {
-        clusters[m].mode = modes[m];
-    }
+        for (int i = 0; i < converged_probes.size(); i++) {
+            Point probe = converged_probes[i];
 
-    for (int i = 0; i < points.size(); i++) {
+            int m = 0;
+            for (; m < modes.size(); m++) {
+                if (euclidean_distance(modes[m], probe) < 2 * epsilon) {
+                    break;
+                }
+            }
 
-        int closest_mode_index = 0;
-        double distance_to_closest_mode = euclidean_distance(points[i], modes[0]);
-        for (int c = 1; c < clusters.size(); c++) {
-            double distance = euclidean_distance(points[i], modes[c]);
-            if (distance < distance_to_closest_mode) {
-                distance_to_closest_mode = distance;
-                closest_mode_index = c;
+            if (m == modes.size()) {
+                modes.push_back(probe);
             }
         }
 
-        clusters[closest_mode_index].support.push_back(points[i]);
+        return modes;
     }
 
-    return clusters;
 }
--- MeanShift.h
+++ MeanShift.h
@@ -2,63 +2,69 @@
 
 #include <vector>
 
-struct Cluster {
-    std::vector<double> mode;
-    std::vector<std::vector<double>> support;
-};
 
-class MeanShift {
-private:
-    double epsilon;
-public:
-    MeanShift() 
-    : epsilon(0.0000001) 
-    { set_kernel(NULL); }
-    
-    MeanShift(double _epsilon) 
-    : epsilon(_epsilon) 
-    { set_kernel(NULL); }
-    
-    MeanShift(double _epsilon, double (*_kernel_func)(double,double))
-    : epsilon(_epsilon)
-    { set_kernel(kernel_func); }
-    
-    // Find clusters using all points as probes
-    std::vector<Cluster> cluster(
-        const std::vector<std::vector<double> > & points, 
-        const double & kernel_bandwidth,
-        const std::vector<std::vector<double> > & probes);
-    
-    // Shift the probes to the modes of the distributions defined by the points 
-    // and the bandwidth until they converge.
-    void meanshift(
-        const std::vector<std::vector<double> > & points, 
-        const double & kernel_bandwidth,
-        std::vector<std::vector<double> > & probes);
-        
-    // Shift the probes to their local modes
-    double shift_probes(
-        const std::vector<std::vector<double> > & points, 
-        const double & kernel_bandwidth,
-        std::vector<std::vector<double> > & probes,
-        std::vector<bool> & stop_moving);
-    
-    // Shift just one probe over the surface defined by the points and bandwidth
-    double shift_probe(
-        const std::vector<std::vector<double> > & points, 
-        const double & kernel_bandwidth,
-        std::vector<double> & probe);
-    
-    // Probes converge to a set of modes.
-    std::vector<std::vector<double> > find_modes(
-        const std::vector<std::vector<double> > & converged_probes);
-    
-    // Create clusters by assigning points to the nearest mode.
-    std::vector<Cluster> cluster(
-        const std::vector<std::vector<double> > & points, 
-        const std::vector<std::vector<double> > & modes);
+namespace meanshift {
 
-private:
-    double (*kernel_func)(double,double);
-    void set_kernel(double (*_kernel_func)(double,double));
-};
+    typedef std::vector<double> Point;
+
+    double euclidean_distance(const Point &point_a, const Point &point_b);
+    double gaussian_kernel(double distance, double kernel_bandwidth);
+
+    class MeanShift {
+    private:
+        double epsilon;
+    public:
+        MeanShift()
+            : epsilon(0.0000001)
+        {
+            set_kernel(NULL);
+        }
+
+        MeanShift(double _epsilon)
+            : epsilon(_epsilon)
+        {
+            set_kernel(NULL);
+        }
+
+        MeanShift(double _epsilon, double(*_kernel_func)(double, double))
+            : epsilon(_epsilon)
+        {
+            set_kernel(kernel_func);
+        }
+
+        // Use meanshift on the probes and return the modes that they converge to
+        std::vector<Point> find_modes(
+            const std::vector<Point> & points,
+            const double & kernel_bandwidth,
+            std::vector<Point> & probes);
+
+        // Shift the probes to the modes of the distributions defined by the points 
+        // and the bandwidth until they converge.
+        void meanshift(
+            const std::vector<Point> & points,
+            const double & kernel_bandwidth,
+            std::vector<Point> & probes);
+
+        // Shift the probes to their local modes
+        double shift_probes(
+            const std::vector<Point> & points,
+            const double & kernel_bandwidth,
+            std::vector<Point> & probes,
+            std::vector<bool> & stop_moving);
+
+        // Shift just one probe over the surface defined by the points and bandwidth
+        double shift_probe(
+            const std::vector<Point> & points,
+            const double & kernel_bandwidth,
+            std::vector<double> & probe);
+
+        // Probes converge to a set of modes.
+        std::vector<Point> find_modes(
+            const std::vector<Point> & converged_probes);
+
+    private:
+        double(*kernel_func)(double, double);
+        void set_kernel(double(*_kernel_func)(double, double));
+    };
+
+}
